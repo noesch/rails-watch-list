732bae52ddf58722fe73e25d94097809e53b3114
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-12 16:53:41 +0200

    implemented the bookmark routes, the controller and the views

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @list = List.find(params[:list_id])
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'New bookmark was created successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
